ffebbb4fbc83e53fd18d4feb99a446f095183f5f
Alex Geis <siegxela@gmail.com>
2022-05-04 08:56:09 -0600

    add / to public path in index.js server file

--- server/index.js
+++ server/index.js
@@ -6,6 +6,6 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname + "public")));
+app.use(express.static(path.join(__dirname + "/public")));
 
 app.listen(PORT);
