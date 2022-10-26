7e0611a9cd01006555b3a3ae84405a13eb623a3c
Alex Geis <siegxela@gmail.com>
2022-10-26 08:10:08 -0600

    add output.clean = true option in webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,48 +3,49 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { ProvidePlugin } = require("webpack");
 
 module.exports = {
-  entry: path.join(__dirname, "src", "index.js"),
-  output: {
-    path: path.resolve(__dirname, "build/"),
-    publicPath: "/",
-    filename: "bundle.js",
-  },
-  mode: process.env.NODE_ENV === "production" ? "production" : "development",
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            // presets: ["@babel/preset-react"],
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|jp(e*)g|svg|gif)$/,
-        use: ["file-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
-    }),
+	entry: path.join(__dirname, "src", "index.js"),
+	output: {
+		publicPath: "/",
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "build/"),
+		clean: true,
+	},
+	mode: process.env.NODE_ENV === "production" ? "production" : "development",
+	performance: {
+		hints: false,
+		maxEntrypointSize: 512000,
+		maxAssetSize: 512000,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						// presets: ["@babel/preset-react"],
+						presets: ["@babel/preset-env", "@babel/preset-react"],
+					},
+				},
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(png|jp(e*)g|svg|gif)$/,
+				use: ["file-loader"],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, "public", "index.html"),
+		}),
 
-    new ProvidePlugin({
-      React: "react",
-    }),
-  ],
+		new ProvidePlugin({
+			React: "react",
+		}),
+	],
 };
